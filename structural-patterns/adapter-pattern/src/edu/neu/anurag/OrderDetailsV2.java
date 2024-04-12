11710af9d538901cae924f62581dff3d36acfbad
nandreanurag <nandre.a@northeastern.edu>
2024-04-12 14:35:04 -0400

    fix wrong implementation of adapater pattern

--- structural-patterns/adapter-pattern/src/edu/neu/anurag/OrderDetailsV2.java
+++ structural-patterns/adapter-pattern/src/edu/neu/anurag/OrderDetailsV2.java
@@ -1,6 +1,6 @@
 package edu.neu.anurag;
 
-public interface OrderDetailsV2 extends OrderDetailsV1{
+public interface OrderDetailsV2{
 
 	void addItem(String itemId);
 	void cancelOrder(String orderNum);
